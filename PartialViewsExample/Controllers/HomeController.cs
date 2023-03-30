f9850317948f5842765f086b587b625cb50e4a56
An <annogo123@gmail.com>
2023-03-30 16:13:08 +0700

    Add project files.

---
+++ PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string>()
+            {
+                "Paris",
+                "New York",
+                "New Dumbai",
+                "Viet Nam"
+            };
+
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            var listModel = new ListModel();
+            listModel.ListTitle = "Programming Languages List";
+            listModel.ListItems = new List<string>() 
+            { 
+               "Java", "C#", "C/C++"     
+            };
+
+            return PartialView("_ListPartialView", listModel);
+        }
+    }
+}
---
+++ PartialViewsExample/Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PartialViewsExample.Models
+{
+    public class ListModel
+    {
+        public string ListTitle { get; set; } = string.Empty;
+        public List<String> ListItems { get; set; } = new List<String>();
+    }
+}
